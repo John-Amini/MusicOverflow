1f98c523fbb8420afd22e4079e755100fe5e6325
John-Amini <john.amini97@gmail.com>
2022-02-03 18:05:46 -0500

    backend done

--- react-project/backend/routes/index.js
+++ react-project/backend/routes/index.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  console.log("hit")
-  res.send('Hello World!');
-});
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   console.log("hit")
+//   res.send('Hello World!');
+// });
 
 module.exports = router;
